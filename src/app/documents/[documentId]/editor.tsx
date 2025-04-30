6d0af5a350ae5172d6261a32608f1641d56c4a1f
Seikida <seikida@hotmail.com>
2025-05-01 03:34:45 +0900

    Bullet List and Ordered List

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -17,6 +17,7 @@ import TextStyle from '@tiptap/extension-text-style';
 import { Color } from '@tiptap/extension-color';
 import Highlight from '@tiptap/extension-highlight';
 import Link from '@tiptap/extension-link';
+import TextAlign from '@tiptap/extension-text-align';
 
 import { useEditorStore } from '@/app/store/use-editor-store';
 
@@ -72,6 +73,9 @@ export const Editor = () => {
             Underline,
             FontFamily,
             TextStyle,
+            TextAlign.configure({
+                types: ['heading', 'paragraph'],
+            }),
             Color,
             Highlight.configure({ multicolor: true }),
             Link.configure({
